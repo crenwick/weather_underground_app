06caac32ee8adb5404c53f917ec424a2aa522396
Charles Renwick <crenwick@crenwick-mac.local>
2014-11-16 21:09:42 -0800

    talks with the api via middleware, responds with json data, and has a static server that grabs the client location and swears at them

---
+++ public/rain.js
@@ -0,0 +1,52 @@
+$( document ).ready(function() {
+
+    var x = document.getElementById("demo");
+    getLocation();
+
+});
+
+//I used information and code from
+//http://www.w3schools.com/html/html5_geolocation.asp
+//to help me access the browser's geolocation ability
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(usePosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function usePosition(position) {
+    var x = document.getElementById("demo");
+    var temperature;
+
+    $.get('http://localhost:3000/api/'+ parseFloat(position.coords.latitude).toFixed(2) + '/'
+          + parseFloat(position.coords.longitude).toFixed(2), function(data){
+        //    $.get('http://localhost:3000/api/47.6/-100', function(data) 
+        //
+        //          {
+        //data.compare: 
+        //1 = raining more there, not here,
+        //2 = raining more in seattle, not there.
+        //3 = not raining at either
+        //0 = raining in both
+        var location = data.You.loc.toUpperCase();
+        x.innerHTML = data.compare;
+
+        if (data.compare === 1) {
+            x.innerHTML = "<h1> UNLIKE " + location + ", ITS NOT RAINING IN SEATTLE, SO FUCK YOU. </h1>";
+        } 
+        else if (data.compare === 2) {
+            x.innerHTML = "<h1> OK YEAH, IT'S RAINING SEATTLE, NOT " + location + ". <BR> FUCK ME. </h1>";
+        } 
+        else if (data.compare === 3) {
+            x.innerHTML = "<h1> IT'S NOT RAINING IN SEATTLE OR IN " + location + ". <BR> BUT FUCK YOU, IT'S MORE BEAUTIFUL HERE. </h1>";
+        } 
+        else if (data.compare === 0) {
+            x.innerHTML = "<h1> RAINING BOTH IN SEATTLE AND " + location + ". <BR> FUCK US BOTH.  </h1>";
+        } 
+        else {
+            x.innerHTML =  "<h1> SOMETHING WENT WRONG. SHIT. </h1>" + data;
+        }
+    });
+}
