5fe352f7e1e6fc09aa7b5fbf386b618869d51071
Charles Renwick <crenwick@crenwick-mac.local>
2014-11-17 19:27:04 -0800

    refactored some code, made a switch, and added a proper test

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*jshint node:true*/
+/*jshint node: true*/
 'use strict';
 
 var express = require('express');
@@ -6,30 +6,27 @@ var express = require('express');
 var app = express();
 var wunder = require('./lib/wunder.js');
 
-app
-.get('/api/:lat/:lon', wunder(), function(req, res) {
-    var compare = 0;
-    if (req.precip > req.precipS) {
-        //raining more there, not seattle
-        compare = 1;
-    }
-    else if (req.precip < req.precipS) {
-        //raining more in seattle, not there
-        compare = 2;
-    } else if (req.precip === 0 && req.precipS === 0) {
-        //not raining in either
-        compare = 3;
-    } // 0 = raining in both?
-    console.log(compare);
-    res.json(
-        {You: {loc: req.loc, temp: req.temp, precip: req.precip},
-         Seattle: {temp: req.tempS, precip: req.precipS},
-         compare: compare});
+app.get('/api/:lat/:lon', wunder(), function(req, res) {
+  var compare;
+  if (req.precip > req.precipSeattle) {
+    compare = 1; //raining more there, not seattle
+  } else if (req.precip < req.precipSeattle) {
+    compare = 2; //raining more in seattle, not there
+  } else if (req.precip !== 0 && req.precipSeattle !== 0) {
+    compare = 3; //raining in both
+  } else {
+    compare = 0; //raining nowhere
+  }
+  console.log(compare);
+  res.json(
+    {You: {loc: req.loc, temp: req.temp, precip: req.precip},
+     Seattle: {temp: req.tempSeattle, precip: req.precipSeattle},
+     compare: compare});
 });
 
 app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
-    console.log('server running on', app.get('port'));
+  console.log('server running on', app.get('port'));
 });
