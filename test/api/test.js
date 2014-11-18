90b85a236430ddb2b9d2f533a4bcfe54fda0f87c
Charles Renwick <crenwick@crenwick-mac.local>
2014-11-17 19:32:18 -0800

    got rid of the console log...

--- test/api/test.js
+++ test/api/test.js
@@ -13,7 +13,6 @@ describe('get two requests from the wunderapi', function() {
     chai.request('http://localhost:3000')
     .get('/api/47.6/-100')
     .end(function(err, res) {
-      console.log(res.body);
       expect(err).to.be.null;
       expect(res.body).to.include.keys(['You', 'Seattle', 'compare']);
       done();
